af99230d4553f0cfac28fde396994e9eb8ea3383
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-29 09:39:30 -0500

    menu

---
+++ Dekevion.py
@@ -0,0 +1,31 @@
+# Create a task list. A user is presented with the text below.
+#Congratulations! You're running [YOUR NAME]'s Task List program.
+
+# What would you like to do next?
+# 1. List all tasks.
+# 2. Add a task to the list.
+# 3. Delete a task.
+# 0. To quit the program
+
+# Let them select an option to list all of their tasks, add a task to their list, delete a task, or quit the program.
+# Make each option a different function in your program.
+# Do <strong>NOT</strong> use Google. Do <strong>NOT</strong> use other students. Try to do this on your own.
+ # What do we need
+
+listArray = ['']
+
+menu = -1
+while menu != 4:
+    menu = int(input('What would you like to do next \n'
+                     '1. List all tasks \n'
+                     '2. Add a task to the list \n'
+                     '3. Delete a task. \n'
+                     '4. To quit the program'))
+    if menu == 1:
+        print('test')
+
+    if menu == 2:
+        print('test')
+
+    if menu == 3:
+        print('test')
